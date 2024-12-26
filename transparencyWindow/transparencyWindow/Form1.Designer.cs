3c4f909b8e77c2a4dcfb5120e3e9dfc0e2045901
Ogurana17 <36149909+Ogurana17@users.noreply.github.com>
2024-12-26 16:41:28 +0900

    Add files via upload

---
+++ transparencyWindow/transparencyWindow/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TransparencyWindow
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.transparencySlider = new System.Windows.Forms.TrackBar();
+            this.transparencyLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.transparencyInput = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.saveHotkeyButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hotkeyInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.transparencySlider)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // transparencySlider
+            // 
+            this.transparencySlider.AutoSize = false;
+            this.transparencySlider.Location = new System.Drawing.Point(18, 64);
+            this.transparencySlider.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.transparencySlider.Name = "transparencySlider";
+            this.transparencySlider.Size = new System.Drawing.Size(324, 45);
+            this.transparencySlider.TabIndex = 0;
+            // 
+            // transparencyLabel
+            // 
+            this.transparencyLabel.AutoSize = true;
+            this.transparencyLabel.Location = new System.Drawing.Point(16, 14);
+            this.transparencyLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.transparencyLabel.Name = "transparencyLabel";
+            this.transparencyLabel.Size = new System.Drawing.Size(35, 12);
+            this.transparencyLabel.TabIndex = 1;
+            this.transparencyLabel.Text = "label1";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(164, 226);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 34);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "button1";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // transparencyInput
+            // 
+            this.transparencyInput.Location = new System.Drawing.Point(212, 40);
+            this.transparencyInput.Name = "transparencyInput";
+            this.transparencyInput.Size = new System.Drawing.Size(130, 19);
+            this.transparencyInput.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(423, 291);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.transparencyLabel);
+            this.tabPage1.Controls.Add(this.saveButton);
+            this.tabPage1.Controls.Add(this.transparencyInput);
+            this.tabPage1.Controls.Add(this.transparencySlider);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(415, 265);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.saveHotkeyButton);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.hotkeyInput);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(415, 265);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // saveHotkeyButton
+            // 
+            this.saveHotkeyButton.Location = new System.Drawing.Point(170, 204);
+            this.saveHotkeyButton.Name = "saveHotkeyButton";
+            this.saveHotkeyButton.Size = new System.Drawing.Size(75, 23);
+            this.saveHotkeyButton.TabIndex = 2;
+            this.saveHotkeyButton.Text = "button1";
+            this.saveHotkeyButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // hotkeyInput
+            // 
+            this.hotkeyInput.Location = new System.Drawing.Point(23, 46);
+            this.hotkeyInput.Name = "hotkeyInput";
+            this.hotkeyInput.Size = new System.Drawing.Size(100, 19);
+            this.hotkeyInput.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 315);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.transparencySlider)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar transparencySlider;
+        private System.Windows.Forms.Label transparencyLabel;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox transparencyInput;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox hotkeyInput;
+        private System.Windows.Forms.Button saveHotkeyButton;
+    }
+}
+
---
+++ transparencyWindow/transparencyWindow/Form1.cs
@@ -0,0 +1,459 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TransparencyWindow
+{
+    public partial class MainForm : Form
+    {
+        private const int HOTKEY_ID = 1;
+        private byte loadTransparency = 224; // デフォルト値: 半透明
+        private byte currentTransparency = 224; // デフォルト値: 半透明
+        private string loadHotkey = "Win+F2"; // 初期値: ショートカットキー
+        private byte nowTransparency = 224;
+        private string nowHotkey = "Win+F2";
+
+        public MainForm()
+        {
+            LoadSettings();
+            InitializeComponent_();
+            IntPtr hwnd = WinAPI.GetForegroundWindow();
+            //currentTransparency = loadTransparency;
+            //SetWindowTransparency(hwnd, currentTransparency); // 起動時に透明度を適用
+        }
+
+        // WinAPI
+        public class WinAPI
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetForegroundWindow();
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+            public const uint LWA_ALPHA = 0x02;
+            public const int GWL_EXSTYLE = -20;
+            public const int WS_EX_LAYERED = 0x80000;
+        }
+
+        // ウィンドウ透明度設定
+        public static void SetWindowTransparency(IntPtr hwnd, byte transparency)
+        {
+            int exStyle = WinAPI.GetWindowLong(hwnd, WinAPI.GWL_EXSTYLE);
+            WinAPI.SetWindowLong(hwnd, WinAPI.GWL_EXSTYLE, exStyle | WinAPI.WS_EX_LAYERED);
+            WinAPI.SetLayeredWindowAttributes(hwnd, 0, transparency, WinAPI.LWA_ALPHA);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // 閉じるボタンを押してもウィンドウを閉じず、タスクトレイに常駐
+            e.Cancel = true;
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+            else
+            {
+                // ウィンドウが表示されたらショートカットキーを再登録
+                RegisterHotKey(loadHotkey);
+            }
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+
+            // ショートカットキーを再登録
+            RegisterHotKey(loadHotkey);
+        }
+
+        // ショートカットキー登録
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            IntPtr hwnd = WinAPI.GetForegroundWindow();
+            InitializeWindowTransparency(hwnd); // 初期化して即時対応
+
+            RegisterHotKey(loadHotkey); // ショートカットキー登録
+
+            // タスクトレイにアイコンを設定
+            NotifyIcon notifyIcon = new NotifyIcon
+            {
+                Icon = SystemIcons.Application,
+                Visible = true,
+                Text = "TransparencyWindow"
+            };
+
+            // タスクトレイのコンテキストメニューを定義
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[]
+            {
+        new MenuItem("Exit", (s, ev) => Application.Exit()) // アプリケーションを終了
+            });
+
+            // ダブルクリックでウィンドウ表示
+            notifyIcon.DoubleClick += (s, ev) =>
+            {
+                this.ShowInTaskbar = true;
+                this.Show();
+                this.WindowState = FormWindowState.Normal; // 最小化を解除
+            };
+
+            // タスクバーから非表示
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide(); // ウィンドウ非表示
+
+            hwnd = WinAPI.GetForegroundWindow();
+            InitializeWindowTransparency(hwnd);
+
+            RegisterHotKey(loadHotkey);
+        }
+
+        private bool IsWindowInitialized(IntPtr hwnd)
+        {
+            return initializedWindows.Contains(hwnd);
+        }
+
+        private void InitializeWindowTransparency(IntPtr hwnd)
+        {
+            //SetWindowTransparency(hwnd, currentTransparency); // 初期透明度を適用
+            initializedWindows.Add(hwnd); // 初期化済みとしてフラグを設定
+        }
+
+        private HashSet<IntPtr> initializedWindows = new HashSet<IntPtr>();
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            if (m.Msg == WM_HOTKEY)
+            {
+                IntPtr hwnd = WinAPI.GetForegroundWindow();
+                ToggleTransparency(hwnd); // 透明化の切り替え
+            }
+            base.WndProc(ref m);
+        }
+
+
+        private void ToggleTransparency(IntPtr hwnd)
+        {
+            byte newTransparency = (byte)(currentTransparency == 255 ? loadTransparency : 255); // 切り替え
+            SetWindowTransparency(hwnd, newTransparency);
+            currentTransparency = newTransparency;
+        }
+
+
+        private void RegisterHotKey(string hotkey)
+        {
+            uint modifier = 0x0000;
+            Keys key = Keys.None;
+
+            string[] keys = hotkey.Split('+');
+            foreach (string part in keys)
+            {
+                switch (part)
+                {
+                    case "Win":
+                        modifier |= 0x0008;
+                        break;
+                    case "Ctrl":
+                        modifier |= 0x0002;
+                        break;
+                    case "Alt":
+                        modifier |= 0x0001;
+                        break;
+                    case "Shift":
+                        modifier |= 0x0004;
+                        break;
+                    default:
+                        key = (Keys)Enum.Parse(typeof(Keys), part);
+                        break;
+                }
+            }
+
+            if (key != Keys.None)
+            {
+                WinAPI.RegisterHotKey(this.Handle, HOTKEY_ID, modifier, (uint)key);
+            }
+        }
+
+        // レジストリ登録（スタート時に起動）
+        private void RegisterStartup()
+        {
+            try
+            {
+                string appPath = Application.ExecutablePath;
+                string appName = "TransparencyWindow";
+
+                using (var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true))
+                {
+                    if (key == null)
+                        throw new Exception("Unable to access the registry key.");
+
+                    key.SetValue(appName, $"\"{appPath}\"");
+                }
+
+                MessageBox.Show("Startup registration successful.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during startup registration: {ex.Message}");
+            }
+        }
+
+        private void UnregisterStartup()
+        {
+            try
+            {
+                string appName = "TransparencyWindow";
+
+                using (var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true))
+                {
+                    if (key == null)
+                        throw new Exception("Unable to access the registry key.");
+
+                    key.DeleteValue(appName, false);
+                }
+
+                MessageBox.Show("Startup unregistration successful.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during startup unregistration: {ex.Message}");
+            }
+        }
+
+
+        // INIファイル操作
+        public static class IniFile
+        {
+            public static void WriteValue(string filePath, string key, string value)
+            {
+                if (!File.Exists(filePath))
+                {
+                    File.WriteAllText(filePath, "");
+                }
+
+                var lines = File.ReadAllLines(filePath);
+                bool keyExists = false;
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].StartsWith(key + "="))
+                    {
+                        lines[i] = key + "=" + value;
+                        keyExists = true;
+                        break;
+                    }
+                }
+
+                if (!keyExists)
+                {
+                    File.AppendAllText(filePath, key + "=" + value + Environment.NewLine);
+                }
+                else
+                {
+                    File.WriteAllLines(filePath, lines);
+                }
+            }
+
+            public static string ReadValue(string filePath, string key)
+            {
+                if (!File.Exists(filePath)) return null;
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    if (line.StartsWith(key + "="))
+                        return line.Substring(key.Length + 1);
+                }
+                return null;
+            }
+        }
+
+        private void SaveSettings()
+        {
+            IniFile.WriteValue("config.ini", "Transparency", nowTransparency.ToString());
+            IniFile.WriteValue("config.ini", "Hotkey", nowHotkey);
+        }
+
+        private void LoadSettings()
+        {
+            string transparencyValue = IniFile.ReadValue("config.ini", "Transparency");
+            if (byte.TryParse(transparencyValue, out byte transparency))
+            {
+                loadTransparency = transparency;
+            }
+
+            string hotkeyValue = IniFile.ReadValue("config.ini", "Hotkey");
+            if (!string.IsNullOrEmpty(hotkeyValue))
+            {
+                loadHotkey = hotkeyValue;
+            }
+
+            // UIの初期値を更新
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox textBox && textBox.Name == "transparencyTextBox")
+                {
+                    textBox.Text = loadTransparency.ToString();
+                }
+            }
+        }
+
+
+        // UI要素の初期化
+        private void InitializeComponent_()
+        {
+            this.Text = "TransparencyWindow";
+            this.Width = 300;
+            this.Height = 200;
+
+            TabControl tabControl1 = new TabControl()
+            {
+                Dock = DockStyle.Fill
+            };
+
+            TabPage tabPage1 = new TabPage("Transparency");
+            TabPage tabPage2 = new TabPage("Shortcut");
+
+            // Transparency settings controls
+            TrackBar transparencySlider = new TrackBar()
+            {
+                Minimum = 1,
+                Maximum = 254,
+                Value = loadTransparency,
+                TickFrequency = 11,
+                SmallChange = 11,
+                LargeChange = 11,
+                Dock = DockStyle.Top
+            }; 
+
+            TextBox transparencyTextBox = new TextBox()
+            {
+                Text = loadTransparency.ToString(),
+                Dock = DockStyle.Top
+            };
+
+            transparencySlider.ValueChanged += (s, e) =>
+            {
+                transparencyTextBox.Text = transparencySlider.Value.ToString();
+                currentTransparency = (byte)transparencySlider.Value;
+                IntPtr hwnd = WinAPI.GetForegroundWindow();
+                //SetWindowTransparency(hwnd, currentTransparency);
+            };
+
+            transparencyTextBox.TextChanged += (s, e) =>
+            {
+                if (byte.TryParse(transparencyTextBox.Text, out byte transparency) && transparency >= 1 && transparency <= 254)
+                {
+                    transparencySlider.Value = transparency;
+                }
+            };
+
+            Label transparencyLabel = new Label()
+            {
+                Text = "Transparency:",
+                Dock = DockStyle.Top
+            };
+
+            tabPage1.Controls.Add(transparencySlider);
+            tabPage1.Controls.Add(transparencyTextBox);
+            tabPage1.Controls.Add(transparencyLabel);
+
+            // Shortcut settings controls
+            Label shortcutLabel = new Label()
+            {
+                Text = "Shortcut Key:",
+                Dock = DockStyle.Top
+            };
+
+            TextBox shortcutTextBox = new TextBox()
+            {
+                Text = loadHotkey,
+                Dock = DockStyle.Top
+            };
+
+            Button saveButton = new Button()
+            {
+                Text = "Save",
+                Dock = DockStyle.Bottom
+            };
+
+            saveButton.Click += (s, e) =>
+            {
+                nowHotkey = shortcutTextBox.Text;
+                nowTransparency = (byte)transparencySlider.Value;
+                SaveSettings();
+                loadHotkey = nowHotkey;
+                RegisterHotKey(loadHotkey);
+                loadTransparency = nowTransparency;
+                //MessageBox.Show("Settings saved successfully.");
+            };
+
+
+            //tabPage2.Controls.Add(shortcutButton);
+            tabPage2.Controls.Add(shortcutTextBox);
+            tabPage2.Controls.Add(shortcutLabel);
+
+            tabControl1.TabPages.Add(tabPage1);
+            tabControl1.TabPages.Add(tabPage2);
+
+            this.Controls.Add(tabControl1);
+            this.Controls.Add(saveButton);
+
+            Button registerTaskButton = new Button()
+            {
+                Text = "Register Task Scheduler",
+                Dock = DockStyle.Bottom
+            };
+
+            registerTaskButton.Click += (s, e) =>
+            {
+                RegisterStartup();
+            };
+
+            Button unregisterTaskButton = new Button()
+            {
+                Text = "Unregister Task Scheduler",
+                Dock = DockStyle.Bottom
+            };
+
+            unregisterTaskButton.Click += (s, e) =>
+            {
+                UnregisterStartup();
+            };
+
+            this.Controls.Add(registerTaskButton);
+            this.Controls.Add(unregisterTaskButton);
+
+        }
+    }
+}
